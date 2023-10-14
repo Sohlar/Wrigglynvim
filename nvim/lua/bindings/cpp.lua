3c37d4626c295b6418f62cfc06bf4a9a90c7aa4b
rvenecia <ryangvenecia@gmail.com>
2023-10-14 11:09:13 -0700

    add compile and profile

--- nvim/lua/bindings/cpp.lua
+++ nvim/lua/bindings/cpp.lua
@@ -5,3 +5,4 @@ vim.keymap.set('n', '<leader>cc', ':!g++ -g main.cpp -o debug<cr>', { remap = tr
 vim.keymap.set('n', '<leader>cr', ':!./debug<cr>', { remap = true, desc = "Compile C++ Debug Binary" })
 vim.keymap.set('n', '<leader>car', ':!g++ -g main.cpp -o debug && ./debug<cr>',
 	{ remap = true, desc = "Compile C++ Debug Binary" })
+vim.keymap.set('n', '<leader>cap', ':!g++ -pg -g -o profile main.cpp && ./profile && gprof profile >> profile.out<cr>', 
